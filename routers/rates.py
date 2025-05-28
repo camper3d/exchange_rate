6e44c46321578b5ce62a191b40b2a9ab7d014f07
camper3d <camper3d@gmail.com>
2025-05-28 17:41:03 +0300

    добавление поиска валюты по символам + интерфейс

--- routers/rates.py
+++ routers/rates.py
@@ -2,10 +2,13 @@ from fastapi import APIRouter, HTTPException
 from services.rates_service import fetch_currency_rates
 from models.currency import CheckCurrency
 from typing import List
+from fastapi import Query
 
 
 router = APIRouter()
 
+
+#
 @router.get('/', response_model=List[CheckCurrency])
 async def get_currency_rates():
     try:
@@ -13,3 +16,12 @@ async def get_currency_rates():
         return rates # показываем курсы
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# поиск валюты по 3 символам
+@router.get('/search', response_model=CheckCurrency)
+async def search_currency(code: str = Query(..., min_length=3,max_length=3)):
+    rates = await fetch_currency_rates() # получаем курсы через функцию
+    for rate in rates:
+        if rate.Cur_Abbreviation.upper() == code.upper(): # если буквы большие,то...
+            return rate # возврат валюты
+    raise HTTPException(status_code=404, detail='Валюта не найдена')
